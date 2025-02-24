dd41e6a9bcdc6e3cda03a1d76a3df4bdd5c203de
mitsuki <miras.mike.s@gmail.com>
2025-02-24 18:08:11 +0800

    try

--- librarysystem.cpp
+++ librarysystem.cpp
@@ -43,21 +43,59 @@ int main(){
 vector<Book> books;
 string title, author, date, genre;
 
-    cout<<"Enter Title: ";
-    getline(cin, title);
+int menuchoice;
+int condition = 0;
+int condition2 = 0;
 
-    cout<<"Enter Author: ";
-    getline(cin, author);
+while(condition = 0){
+    cout<<"Menu"<<endl;
+    cout<<"1 - Add Book"<<endl;
+    cout<<"2 - Display Books"<<endl;
+    cout<<"3 - Exit"<<endl;
+    cin>>menuchoice;
 
-    cout<<"Enter Release Date: ";
-    getline(cin, date);
+switch(menuchoice){
+    case 1:
+        while(condition2 = 0){
+            char addanother = 'y';
+            cout<<"Enter Title: ";
+            getline(cin, title);
 
-    cout<<"Enter Genre: ";
-    getline(cin, genre);
+            cout<<"Enter Author: ";
+            getline(cin, author);
 
-    books.push_back(Book(title, author, date, genre));
+            cout<<"Enter Release Date: ";
+            getline(cin, date);
 
-    books[0].display();
+            cout<<"Enter Genre: ";
+            getline(cin, genre);
 
+            books.push_back(Book(title, author, date, genre));
+
+            cout<<"Would you like to add another book? (y/n)";
+            cin>>addanother;
+
+            if(addanother == 'n' || 'N'){
+                condition2 = 1;
+            }
+        }
+        break;
+
+    case 2:
+        for(int i = 0; i < books.size(); i++){
+            books[i].display();
+        }
+        break;
+
+    case 3:
+        condition = 1;
+        break;
+}   
+
+    
+
+
+   
+}
 return 0;
 }
